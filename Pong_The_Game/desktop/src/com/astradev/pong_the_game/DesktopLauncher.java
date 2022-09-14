0d8243e021a51ff867d9655bd0aeae40db6ce9a0
MalcolnLMR <malcolnlucas@gmail.com>
2022-09-14 16:57:04 -0300

    Pong First Commit

---
+++ Pong_The_Game/desktop/src/com/astradev/pong_the_game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.astradev.pong_the_game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.astradev.pong_the_game.Pong;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Pong_The_Game");
+		new Lwjgl3Application(new Pong(), config);
+	}
+}
